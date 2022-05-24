b1e172243c8fda7f355ff72a31064c45b28220d1
Gwakamaul <106025859+Gwakamaul@users.noreply.github.com>
2022-05-24 08:07:46 +0200

    Initial commit

---
+++ src/main/java/net/gwakamaul/training_mod/mixin/ExampleMixin.java
@@ -0,0 +1,19 @@
+package net.gwakamaul.training_mod.mixin;
+
+import net.gwakamaul.training_mod.TrainingMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+
+	@Inject(at = @At("HEAD"), method = "init()V")
+
+	private void init(CallbackInfo info) {
+		TrainingMod.LOGGER.info("This line is printed by an example mod mixin!");
+
+	}
+}
